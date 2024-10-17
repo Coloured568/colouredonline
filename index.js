31b6ec29826c4fca1790ac6c3c6ff06d90c89f7e
_Coloured <79119198+Coloured568@users.noreply.github.com>
2024-10-16 22:14:23 -0700

    Update index.js
    added discovery

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+'use strict';
+
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const startDB = require('./startdb');
 const authRouter = require('./auth');
 const editorRouter = require('./editor');
@@ -7,13 +11,15 @@ const redirectIfNotVerified = require('./middleware/verified');
 const authUser = require('./middleware/authUser');
 const authVerifier = require('./middleware/authenticated');
 const subdomainHandler = require('./domains');
+const discoverRoute = require('./discover');
 require('dotenv').config();
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 db = startDB.db;
 startDB.setupDB();
 
 const app = express();
+
 app.use(express.static('public'));
 app.engine('art', require('express-art-template'));
 app.set("views", "./views");
@@ -29,25 +35,26 @@ app.use(authVerifier);
 app.use(authUser);
 
 app.get('/', async (req, res) => {
-    res.render('index', { title: 'Home', url: process.env.URL});
+    res.render('index', { title: 'Home', url: process.env.URL });
 });
-
 app.get('/tos', async (req, res) => {
-    res.render('tos', { title: 'Terms of Service', url: process.env.URL});
+    res.render('tos', { title: 'Terms of Service', url: process.env.URL });
+}); 
+
+/* app.use('/discover', discoverRoute); */-
+
+app.get('/discover', async (req, res) => {
+    res.render('discover', { title: 'Discovery', url: process.env.URL })
 });
 
 app.get('/privacy', async (req, res) => {
-    res.render('privacy', { title: 'Privacy Policy', url: process.env.URL});
+    res.render('privacy', { title: 'Privacy Policy', url: process.env.URL });
 });
-
 app.use('/auth', authRouter);
-
 app.use('/editor', authToken, redirectIfNotVerified, editorRouter);
-
 app.get('/verified', (req, res) => {
     res.render('verified', { title: 'Verified', url: process.env.URL });
 });
-
 app.use('/dashboard', authToken, redirectIfNotVerified, require('./dashboard'));
 
 // 404 Error Handler
